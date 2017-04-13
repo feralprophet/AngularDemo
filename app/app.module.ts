359cf781a71fe08cd1b4800a9b5604e4294ea899
feralprophet <veudain@gmail.com>
2017-04-13 15:25:09 -0700

    Added routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,14 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import {ProductFilterPipe} from "./product/product-list.pipe";
 import {StarComponent} from "./shared/star.component";
 import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import {ProductListComponent} from './product/product-list.component'
+import {WelcomeComponent} from './home/welcome.component';
+import {ProductDetailComponent} from './product/product-detail.component';
+
 @NgModule({
-    imports: [BrowserModule, FormsModule, HttpModule],
-  declarations: [AppComponent,
+    imports: [BrowserModule, FormsModule, HttpModule,
+        RouterModule.forRoot([
+            { path: 'products', component: ProductListComponent },
+            { path: 'product/:id', component: ProductDetailComponent },
+            { path: 'welcome', component: WelcomeComponent },
+            {path: '', redirectTo:'welcome', pathMatch:'full'},
+            {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+        ])],
+    declarations: [AppComponent,
+        WelcomeComponent,
+        ProductDetailComponent,
         ProductListComponent,
-      ProductFilterPipe, StarComponent],
+        ProductFilterPipe, StarComponent    ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
